bd92989a846ba3a81cdb8e32b9bf110ae9ae512f
MikeSzabla <ms2667@njit.edu>
2021-10-20 13:56:46 -0400

    adds multiply methods and tests

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -6,6 +6,7 @@ def test_calculator_result():
     calc = Calculator()
     assert calc.result == 0
 
+
 def test_calculator_add():
     """Testing the Add function of the calculator"""
     #Arrange by instantiating the calc class
@@ -15,14 +16,32 @@ def test_calculator_add():
     #Assert that the results are correct
     assert calc.result == 1
 
+
 def test_calculator_get_result():
     """Testing the Get result method of the calculator"""
     calc = Calculator()
     calc.add_number(1)
     assert calc.get_result() == 1
 
+
 def test_calculator_subtract():
     """Testing the subtract method of the calculator"""
     calc = Calculator()
     calc.subtract_number(1)
     assert calc.get_result() == -1
+
+
+def test_calculator_multiply():
+    """Testing the multiply method of the calculator"""
+    calc = Calculator()
+    calc.add_number(2)
+    calc.multiply_number(2)
+    assert calc.get_result() == 4
+
+
+def test_calculator_divide():
+    """Testing the divide method of the calculator"""
+    calc = Calculator()
+    calc.add_number(2)
+    calc.divide_number(2)
+    assert calc.get_result() == 1
