a46651d0b67d7cec686a139c741af74a36905dda
thulasigovardhan <thulasigovardhan@gmail.com>
2021-11-02 19:54:05 -0400

    Task: Created classes, objects and methods to perform the different operations with saving the results in an array and their respective unit tests

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,25 +1,59 @@
 """ This is the increment function"""
-class Calculator:
+from calc.addition import Addition
+from calc.subtraction import Subtraction
+from calc.multiplication import Multiplication
+from calc.division import Division
+
+class   Calculator:
     """ This is the Calculator class"""
 
     history = []
 
     @staticmethod
+    def add_calculation_to_history(calculation):
+        Calculator.history.append(calculation)
+
+    @staticmethod
+    def get_last_calculation_result():
+        # -1 index gets the last item added to history
+        return Calculator.history[-1].getResult()
+
+    @staticmethod
+    def get_last_calculation_object():
+        return Calculator.history[-1]
+
+    @staticmethod
+    def clear_history():
+        Calculator.history.clear()
+        return Calculator.count_history()
+
+    @staticmethod
+    def count_history():
+        return len(Calculator.history)
+
+    @staticmethod
     def add_number(value_a, value_b):
-        """ adds number to result"""
-        return value_a + value_b
+        """ Instantiating  Addition object and passing value a and value b to the constructor"""
+        #This is using a factory create method to return an instance of the class
+        Calculator.add_calculation_to_history(Addition.create(value_a,value_b))
+        return Calculator.get_last_calculation_result()
 
     @staticmethod
     def subtract_number(value_a, value_b):
         """ subtract number from result"""
-        return value_a - value_b
+        Calculator.add_calculation_to_history(Subtraction.create(value_a,value_b))
+        return Calculator.get_last_calculation_result()
 
     @staticmethod
     def multiply_numbers( value_a, value_b):
         """ multiply two numbers and store the result"""
-        return value_a * value_b
+        Calculator.add_calculation_to_history(Multiplication.create(value_a,value_b))
+        return Calculator.get_last_calculation_result()
 
     @staticmethod
     def divide_numbers( value_a, value_b):
         """ divide two numbers and store the result"""
-        return value_a / value_b
+        Calculator.add_calculation_to_history(Division.create(value_a, value_b))
+        return Calculator.get_last_calculation_result()
+
+
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -2,51 +2,64 @@
 import pytest
 from calculator.calculator import Calculator
 
-def test_calculator_add():
+# This is how you define a function that will run each time yuo pass it to a test, it is called a fixture
+@pytest.fixture
+def clear_history():
+    Calculator.clear_history()
+
+def test_calculator_add_first(clear_history):
     """Testing the Add function of the calculator"""
-    # Arrange
-    value_a = 1
-    value_b = 2
-    # Act
-    result = Calculator.add_number(value_a,value_b)
-    # Assert
-    assert result == 3
+    assert Calculator.add_number(1, 2) == 3
 
-def test_calculator_subtract():
+def test_calculator_add_second(clear_history):
+    """Testing the Add function of the calculator"""
+    assert Calculator.add_number(2, 3) == 5
+
+def test_calculator_add_third(clear_history):
+    """Testing the Add function of the calculator"""
+    assert Calculator.add_number(3, 4) == 7
+
+def test_calculator_history_count(clear_history):
+    assert Calculator.count_history() == 0
+
+def test_calculator_clear_history():
+    assert Calculator.clear_history() == 0
+
+def test_calc_complete(clear_history):
+    #The calculator is holding static properties and methods i.e history property there is only one history property
+    #Addition calculation objects to the history
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2,3) == 5
+    assert Calculator.add_number(3,4) == 7
+    assert Calculator.count_history() == 3
+    #The numbers in the [] refers to the calculation obj ect in that position and you can call the method on the object
+    assert Calculator.history[0].getResult() == 3
+    assert Calculator.history[1].getResult() == 5
+    assert Calculator.history[2].getResult() == 7
+
+def test_calculator_subtract(clear_history):
     """Testing the subtract method of the calculator"""
-    # Arrange
-    value_a = 1
-    value_b = 2
-    # Act
-    result = Calculator.subtract_number(value_a, value_b)
-    # Assert
-    assert result == -1
+    assert Calculator.subtract_number(1, 2) == -1
+    assert Calculator.subtract_number(2, 4) == -2
+    assert Calculator.subtract_number(3, 6) == -3
+    assert Calculator.count_history() == 3
 
-def test_calculator_multiply():
+def test_calculator_multiply(clear_history):
     """ Testing multiplication of two numbers"""
-    # Arrange
-    value_a = 1
-    value_b = 2
-    # Act
-    result = Calculator.multiply_numbers(value_a, value_b)
-    # Assert
-    assert result == 2
+    assert Calculator.multiply_numbers(1, 2) == 2
+    assert Calculator.multiply_numbers(2, 3) == 6
+    assert Calculator.multiply_numbers(3, 4) == 12
+    assert Calculator.multiply_numbers(4, 5) == 20
+    assert Calculator.count_history() == 4
+    assert Calculator.clear_history() == 0
 
-def test_calculator_division():
+def test_calculator_division(clear_history):
     """ Testing division of two numbers"""
-    # Arrange
-    value_a = 1
-    value_b = 1
-    # Act
-    result = Calculator.divide_numbers(value_a, value_b)
-    # Assert
-    assert result == 1
+    assert Calculator.divide_numbers(1,1) == 1
+    assert Calculator.divide_numbers(2,1) == 2
+    assert Calculator.divide_numbers(3,1) == 3
 
 def test_calculator_division_exception():
     """ Testing division exception for division by zero"""
-    # Arrange
-    value_a = 1
-    value_b = 0
-    # Act
     with pytest.raises(ZeroDivisionError):
-        Calculator.divide_numbers(value_a,value_b)
+        Calculator.divide_numbers(1,0)
