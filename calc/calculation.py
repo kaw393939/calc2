7b4560dfcfe7e2bdc8b191c9d91f3599037735ac
thulasigovardhan <thulasigovardhan@gmail.com>
2021-11-04 12:34:43 -0400

    Task: Mirrored my code to Professor's code and cleared the pylint issues

--- calc/calculation.py
+++ calc/calculation.py
@@ -1,11 +1,15 @@
-"""This is going to be the Calculation object"""
-
+"""This is our calculation base class / Abstract Class"""
 class Calculation:
     """Initializing objects"""
+
+    # self references the instantiated object of the class
+    # these are instance properties that are being sharred
+    # with the child classes (addition, subtraction, et
     def __init__(self, value_a, value_b):
         self._value_a = value_a
         self._value_b = value_b
 
+    # Class Factory Method <- bound to the class and not the instance of the class
     @classmethod
     def create(cls, value_a, value_b):
         """Using class method to create objects of all individual operations"""
--- calculator/calculator.py
+++ calculator/calculator.py
@@ -10,26 +10,40 @@ class   Calculator:
     history = []
 
     @staticmethod
+    def get_result_of_first_calculation_added_to_history():
+        """Adding the first result to the history array"""
+        return Calculator.history[0].get_result()
+
+    @staticmethod
     def add_calculation_to_history(calculation):
         """Adding the results to the history array"""
         Calculator.history.append(calculation)
+        return True
 
     @staticmethod
-    def get_last_calculation_result():
-        """Getting the latest result from history array"""
-        # -1 index gets the last item added to history
+    def get_result_of_last_calculation_added_to_history():
+        """Adding the last result to the history array"""
+        # -1 gets the last item added to the list automatically
+        # and you can expect it to have the get result method
         return Calculator.history[-1].get_result()
 
-    @staticmethod
-    def get_last_calculation_object():
-        """Creating object for the latest result in the history array"""
-        return Calculator.history[-1]
+    # @staticmethod
+    # def get_last_calculation_result():
+    #     """Getting the latest result from history array"""
+    #     # -1 index gets the last item added to history
+    #     return Calculator.history[-1].get_result()
+
+    # @staticmethod
+    # def get_last_calculation_object():
+    #     """Creating object for the latest
+    #     result in the history array"""
+    #     return Calculator.history[-1]
 
     @staticmethod
     def clear_history():
         """Clearing the results history array and getting the count of the history indexes"""
         Calculator.history.clear()
-        return Calculator.count_history()
+        return True
 
     @staticmethod
     def count_history():
@@ -38,25 +52,37 @@ class   Calculator:
 
     @staticmethod
     def add_number(value_a, value_b):
-        """ Instantiating  Addition object and passing value a and value b to the constructor"""
-        #This is using a factory create method to return an instance of the class
-        Calculator.add_calculation_to_history(Addition.create(value_a,value_b))
-        return Calculator.get_last_calculation_result()
+        """ adds number to result"""
+        # create an addition object using the
+        # factory we created on the calculation class
+        addition = Addition.create(value_a, value_b)
+        # addition = Addition(value_a,value_b)
+        # <-this is not good but will work.  It will be repeated too much
+        Calculator.add_calculation_to_history(addition)
+        return Calculator.get_result_of_last_calculation_added_to_history()
 
     @staticmethod
+    #this is an example of a calling method
     def subtract_number(value_a, value_b):
         """ subtract number from result"""
-        Calculator.add_calculation_to_history(Subtraction.create(value_a,value_b))
-        return Calculator.get_last_calculation_result()
+        # create an subtraction object using the
+        # factory we created on the calculation class
+        subtraction = Subtraction.create(value_a, value_b)
+        # addition = Addition(value_a,value_b)
+        # <-this is not good but will work.  It will be repeated too much
+        Calculator.add_calculation_to_history(subtraction)
+        return Calculator.get_result_of_last_calculation_added_to_history()
 
     @staticmethod
-    def multiply_numbers( value_a, value_b):
+    def multiply_numbers(value_a, value_b):
         """ multiply two numbers and store the result"""
+        #this is a shorthand way to create the
+        # multiplication object and added it the history in one line
         Calculator.add_calculation_to_history(Multiplication.create(value_a,value_b))
-        return Calculator.get_last_calculation_result()
+        return Calculator.get_result_of_last_calculation_added_to_history()
 
     @staticmethod
     def divide_numbers( value_a, value_b):
         """ divide two numbers and store the result"""
         Calculator.add_calculation_to_history(Division.create(value_a, value_b))
-        return Calculator.get_last_calculation_result()
+        return Calculator.get_result_of_last_calculation_added_to_history()
