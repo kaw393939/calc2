4b48d038421436bd6de33b5d5364368f5a5914a9
sravyamyla <90294460+sravyamyla@users.noreply.github.com>
2021-11-04 20:42:58 -0400

    Update calculator.py

--- calc/calculator.py
+++ calc/calculator.py
@@ -1,16 +1,13 @@
-""" This is the increment function"""
-from calc.operations.addition import Addition
-from calc.operations.subtraction import Subtraction
+"""This is our calculation base class / Abstract Class"""
+class Calculation:
 
-class Calculator:
-    """ This is the Calculator class"""
-
-    @staticmethod
-    def add_numbers(value_a, value_b):
-        """ adds two numbers"""
-        return Addition.add(value_a, value_b)
-
-    @staticmethod
-    def subtract_numbers(value_a, value_b):
-        """ subtract number from result"""
-        return Subtraction.subtract(value_a,value_b)
+    #contstructor and it is the first function called when an object of the class is instantiated
+    def __init__(self,value_a, value_b):
+        #self references the instantiated object of the class
+        #these are instance properties that are being sharred with the child classes (addition, subtraction, etc...)
+        self.value_a = value_a
+        self.value_b = value_b
+    # Class Factory Method <- bound to the class and not the instance of the class
+    @classmethod
+    def create(cls, value_a, value_b):
+        return cls(value_a,value_b)
