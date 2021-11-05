814f912150be4c0aafeb1799fa07d6fafc3500d7
sravyamyla <90294460+sravyamyla@users.noreply.github.com>
2021-11-04 22:01:56 -0400

    Delete subtraction_test.py

--- tests/subtraction_test.py
+++
@@ -1,6 +0,0 @@
-"""Testing Addition"""
-from calc.operations.subtraction import Subtraction
-
-def test_subtraction():
-    """testing calc result is -1"""
-    assert Subtraction.subtract(1,2) == -1
