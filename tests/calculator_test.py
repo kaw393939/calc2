c00348481ca70851d44667ff2bebfaa665460392
sravyamyla <90294460+sravyamyla@users.noreply.github.com>
2021-11-04 22:02:25 -0400

    Update calculator_test.py

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -11,18 +11,28 @@ def test_calculator_add():
     #Arrange by instantiating the calc class
     calc = Calculator()
     #Act by calling the method to be tested
-    calc.add_number(1)
+    calc.add_number(4)
     #Assert that the results are correct
-    assert calc.result == 1
+    assert calc.result == 4
 
 def test_calculator_get_result():
     """Testing the Get result method of the calculator"""
     calc = Calculator()
-    calc.add_number(1)
-    assert calc.get_result() == 1
+    assert calc.get_result() == 0
 
 def test_calculator_subtract():
     """Testing the subtract method of the calculator"""
     calc = Calculator()
     calc.subtract_number(1)
     assert calc.get_result() == -1
+def test_calculator_multiply():
+    """ tests multiplication of two numbers"""
+    calc = Calculator()
+    result  = calc.multiply_numbers(1,2)
+    assert result == 2
+
+def test_calculator_divide():
+    """ tests multiplication of two numbers"""
+    calc = Calculator()
+    result  = calc.divide_numbers(1,2)
+    assert result == 0.5
