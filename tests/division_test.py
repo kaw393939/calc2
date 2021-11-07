679c6e70ea2882a988b319fa800b82748f838d8f
thulasigovardhan <thulasigovardhan@gmail.com>
2021-11-07 12:50:27 -0500

    Task: Added different values and different length of values to the tuple and test the operations

--- tests/division_test.py
+++ tests/division_test.py
@@ -5,16 +5,16 @@ from calc.calculations.division import Division
 def test_calculation_division():
     """testing that our calculator has a static method for multiplication"""
     #Arrange
-    mynumbers = (1.0,2.0)
+    mynumbers = (1.0,2.0,4.0)
     division = Division(mynumbers)
     #Act
     #Assert
-    assert division.get_result() == 0.5
+    assert division.get_result() == 0.125
 
 def test_calculator_division_exception():
     """ Testing division exception for division by zero"""
     # Arrange
-    mynumbers = (1.0, 0.0)
+    mynumbers = (1.0, 0.0, 1.0)
     division = Division(mynumbers)
     # Act
     # Assert
--- tests/multiplication_test.py
+++ tests/multiplication_test.py
@@ -4,8 +4,8 @@ from calc.calculations.multiplication import Multiplication
 def test_calculation_multiplication():
     """testing that our calculator has a static method for multiplication"""
     #Arrange
-    mynumbers = (1.0,2.0)
+    mynumbers = (1.0,2.0,3.0)
     multiplication = Multiplication(mynumbers)
     #Act
     #Assert
-    assert multiplication.get_result() == 2.0
+    assert multiplication.get_result() == 6.0
