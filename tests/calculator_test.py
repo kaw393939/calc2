1e7613f2c0c4f97ab51395447dda8261851f2370
goatz18 <jg852@njit.edu>
2021-11-10 18:23:28 -0500

    Edit: Zero division fun

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -40,10 +40,19 @@ def test_calculator_multiply():
     assert result == 2
 
 
-def test_divison():
+def test_division():
     """Calls the calculator class from main.py"""
     calc = Calculator()
     # Calls the division function from main.py and inputs static number.
     result = calc.division(6, 3)
     # Assert that the results are correct
     assert result == 2
+
+
+def test_division_zero():
+    """Calls the calculator class from main.py"""
+    calc = Calculator()
+    # Calls the division function from main.py and inputs static number.
+    result = calc.division(6, 0)
+    # Assert that the results are correct
+    assert result == ZeroDivisionError
