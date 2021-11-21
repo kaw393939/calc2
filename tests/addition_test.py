ab5b2d4cc9d4947405180877e043fec95028ca49
thulasigovardhan <thulasigovardhan@gmail.com>
2021-11-21 15:12:17 -0500

    Task: Enhanced AAA testing to test from external file source

--- tests/addition_test.py
+++ tests/addition_test.py
@@ -1,12 +1,26 @@
 """Testing Addition"""
+import os
+
 import pandas as pd
+
 from calc.calculations.addition import Addition
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__)) # Get Current Working Directory
+
 
 def test_calculation_addition():
     """testing that our calculator has a static method for addition"""
     #Arrange
-    numbers = (1.0,2.0,3.0,4.0)
-    #Act
-    addition = Addition.create(numbers)
-    #Assert
-    assert addition.get_result() == 10.0
+    file_path = os.path.join(BASE_DIR, "addition_15values.xlsx")
+    df = pd.read_excel(io=file_path, index_col=None, names=["value_1",	"value_2",	"result"], dtype='float')
+    for index, row in df.iterrows():
+        tuple_values = (row.value_1, row.value_2)
+        #print(tuple_values)
+        addition = Addition.create(tuple_values)
+        assert addition.get_result() == df['result'][index]
+
+    with pd.ExcelWriter('addition_15values.xlsx', mode='a') as writer:
+        df.to_excel(writer, sheet_name='Tested_output', index=False)
+        writer.save()
+
+
