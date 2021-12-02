41eb84efba2c8266b866f28c54861ed39d3889b3
kaw393939 <keith@webizly.com>
2021-12-02 14:39:01 -0500

    Updated to use controllers

---
+++ app/controllers/index_controller.py
@@ -0,0 +1,9 @@
+from app.controllers.controller import ControllerBase
+from flask import render_template, request
+
+class IndexController(ControllerBase):
+    @staticmethod
+    def index():
+        return render_template('index.html')
+
+
