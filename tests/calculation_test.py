db04c016ef283897f66e11fd165c1791c66c17f4
LuisB3 <luis.bastidas@gmail.com>
2021-12-09 17:18:48 -0500

    minor updates and testing

--- tests/calculation_test.py
+++ tests/calculation_test.py
@@ -1,5 +1,6 @@
 """Testing calculation class"""
 import pytest
+import pprint
 from calc.history.calculator_result import CalculatorResult
 from calc.calculations.addition import Addition
 
@@ -28,29 +29,3 @@ def test_clear_calculation_history(clear_history_fixture, setup_addition_calcula
     CalculatorResult.clear_history()
     assert CalculatorResult.count_history() == 0
     assert CalculatorResult.clear_history() == True
-
-#def test_get_calculation(clear_history_fixture, setup_addition_calculation_fixture):
-#    """Get calculation from history"""
-#    # pylint: disable=unused-argument,redefined-outer-name
-#    assert CalculatorResult.get_calculation(0).get_result() == 3
-#
-#def test_get_calc_last_result_value(clear_history_fixture, setup_addition_calculation_fixture):
-#    """Get last calculation from history"""
-#    # pylint: disable=unused-argument,redefined-outer-name
-#    assert CalculatorResult.get_last_calculation_result_value() == 3
-#
-#def test_get_calculation_first(clear_history_fixture, setup_addition_calculation_fixture):
-#    """Get first calculation from history"""
-#    # pylint: disable=unused-argument,redefined-outer-name
-#    assert CalculatorResult.get_first_calculation().get_result() == 3
-#
-#def test_history_count(clear_history_fixture, setup_addition_calculation_fixture):
-#    """Get calculation count from history"""
-#    # pylint: disable=unused-argument,redefined-outer-name
-#    assert CalculatorResult.count_history() == 1
-#
-#def test_get_calc_last_result_object(clear_history_fixture, setup_addition_calculation_fixture):
-#    """Get last calculation from  history"""
-#    # pylint: disable=unused-argument,redefined-outer-name
-#    #This test if it returns the last calculation as an object
-#    assert isinstance(CalculatorResult.get_last_calculation_object(), Addition)
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,7 +1,8 @@
 """Testing the Calculator"""
 import pytest
-import pandas as pd
+import pprint
 
+from calc.calculator import Calculator
 from calc.history.calculator_result import CalculatorResult
 from calc.calculations.division import Division
 from calc.calculations.addition import Addition
@@ -10,40 +11,51 @@ from calc.calculations.subtraction import Subtraction
 from calc.utilities.absolutepath import absolutepath
 
 @pytest.fixture
-def clear_history_fixture():
-    """fixture to run tests"""
-    # pylint: disable=redefined-outer-name
-    CalculatorResult.clear_history()
+def clear_history():
+    Calculator.clear_history()
 
-def test_calculator_add_static(clear_history_fixture):
-    """testing that our calculator has a static method for addition"""
-    csv_reader = pd.read_csv(absolutepath("tests/files/addition.csv"))
-    for index, row in csv_reader.iterrows():
-        addition = Addition.create(row.value1, row.value2)
-        addition_result = csv_reader["result"][index]
-        assert addition.get_result() == addition_result
+def test_calculator_add(clear_history):
+    """testing the add function of the calculator"""
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(4, 2) == 6
+    assert Calculator.history_count() == 4
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 6
+    pprint.pprint(Calculator.history)
 
-def test_calculator_subtract_static(clear_history_fixture):
-    """Testing the subtract method of the calc"""
-    csv_reader = pd.read_csv(absolutepath("tests/files/subtraction.csv"))
-    for index, row in csv_reader.iterrows():
-        subtraction = Subtraction.create(row.value1, row.value2)
-        subtraction_result = csv_reader["result"][index]
-        assert subtraction.get_result() == subtraction_result
+def test_clear_history(clear_history):
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(4, 2) == 6
+    assert Calculator.history_count() == 4
+    assert Calculator.clear_history() == True
+    assert Calculator.history_count() == 0
 
-def test_calculator_multiply_static(clear_history_fixture):
-    """Testing the multiplication method of the calc"""
-    csv_reader = pd.read_csv(absolutepath("tests/files/multiplication.csv"))
-    for index, row in csv_reader.iterrows():
-        multiplication = Multiplication.create(row.value1, row.value2)
-        multiplication_result = csv_reader["result"][index]
-        assert multiplication.get_result() == multiplication_result
+def test_count_history(clear_history):
+    assert Calculator.history_count() == 0
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.history_count() == 2
 
-def test_calculator_divide_static(clear_history_fixture):
-    """Testing the division method of the calc"""
-    csv_reader = pd.read_csv(absolutepath("tests/files/division.csv"))
-    for index, row in csv_reader.iterrows():
-        division = Division.create(row.value1, row.value2)
-        division_result = csv_reader["result"][index]
-        assert division.get_result() == division_result
+def test_get_last_calculator_result(clear_history):
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 5
 
+def test_get_first_calculator_result(clear_history):
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.get_result_of_first_calculation_added_to_history() == 4
+
+def test_calculator_subtract(clear_history):
+    """Testing the subtract method of the calculator"""
+    assert Calculator.subtract_number(1,2) == -1
+
+def test_calculator_multiply(clear_history):
+    """test multiplication of two numbers"""
+    assert Calculator.multiply_numbers(1,2) == 2
+def test_calculator_divide(clear_history):
+    """test division of two numbers"""
+    assert Calculator.divide_numbers(4,2) == 2
