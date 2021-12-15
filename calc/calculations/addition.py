7e36599385848d0575f0e2d14c621d79806737cd
Darshan Patel <44410769+imdp7@users.noreply.github.com>
2021-12-15 07:32:51 -0500

    Update addition.py

--- calc/calculations/addition.py
+++ calc/calculations/addition.py
@@ -5,7 +5,6 @@ class Addition(Calculation):
     """ calculation addition class"""
     def get_result(self):
         """get the addition results"""
-        sum_of_values = 0.0
-        for value in self.values:
-            sum_of_values = value + sum_of_values
-        return sum_of_values
+        # pylint dia
+        return sum(self.values)
+    
