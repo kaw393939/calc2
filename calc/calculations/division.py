73a9c6f06d2386d9285e7a577e253eecceeb4125
Darshan Patel <44410769+imdp7@users.noreply.github.com>
2021-12-15 07:32:04 -0500

    Update division.py

--- calc/calculations/division.py
+++ calc/calculations/division.py
@@ -3,13 +3,12 @@ from calc.calculations.calculation import Calculation
 
 class Division(Calculation):
     """Division calculation object"""
-
     def get_result(self):
-      try:
-        """ divide two numbers and store the result"""
-        result = 1.0
-        for value in self.values:
-            result = result / value
-        return result
-      except ZeroDivisionError:
-            print("Divide by Zero")
+        """get the division results"""
+        #initial_value = 1.0
+        for index, value in enumerate(self.values):
+            if index == 0:
+                division_value = value
+            else:
+                division_value = division_value / value
+        return round(division_value, 5)
