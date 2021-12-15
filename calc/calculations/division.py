dafaa56454b9f28a96d197248cc43ed4839ce243
Darshan Patel <44410769+imdp7@users.noreply.github.com>
2021-12-15 07:23:48 -0500

    Update division.py

--- calc/calculations/division.py
+++ calc/calculations/division.py
@@ -13,3 +13,15 @@ class Division(Calculation):
         return result
       except ZeroDivisionError:
             print("Divide by Zero")
+            
+"""Addition Class"""
+from calc.calculations.calculation import Calculation
+
+class Addition(Calculation):
+    """ calculation addition class"""
+    def get_result(self):
+        """get the addition results"""
+        sum_of_values = 0.0
+        for value in self.values:
+            sum_of_values = value + sum_of_values
+        return sum_of_values
