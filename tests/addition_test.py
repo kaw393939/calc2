d8b99eab9cdca002a5e205db157cd286f8960246
Darshan Patel <44410769+imdp7@users.noreply.github.com>
2021-12-15 07:35:25 -0500

    Update addition_test.py

--- tests/addition_test.py
+++ tests/addition_test.py
@@ -4,20 +4,8 @@ from calc.calculations.addition import Addition
 def test_calculation_addition():
     """testing that our calculator has a static method for addition"""
     #Arrange
-    mynumbers = (1.0,2.0)
+    mynumbers = (1.0,2.0,3.0,4.0)
     addition = Addition(mynumbers)
     #Act
     #Assert
-    assert addition.get_result() == 3.0
-    #Arrange
-    mynumbers = (2.0,2.0)
-    addition = Addition(mynumbers)
-    #Act
-    #Assert
-    assert addition.get_result() == 4.0
-    #Arrange
-    mynumbers = (0.0,2.0)
-    addition = Addition(mynumbers)
-    #Act
-    #Assert
-    assert addition.get_result() == 2.0
+    assert addition.get_result() == 10.0
